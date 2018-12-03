931deaf485f34b71befba2eb948b881372ad13b6
JIA POTEMPA <rl107520@rlas-macair-26.rlas-116.org>
2018-12-03 10:49:11 -0600

    Initial Commit

---
+++ RockPaperScissor's/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissor's
+//
+//  Created by JIA POTEMPA on 12/3/18.
+//  Copyright © 2018 JIA POTEMPA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
